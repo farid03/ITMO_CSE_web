a29198f22b03ffdaa0f8ef7dbe82cf4a28dd9e4c
Farid Kurbanov <71608691+farid03@users.noreply.github.com>
2022-01-12 18:29:15 +0300

    delete web4

--- web4/src/main/java/ru/ifmo/se/web4/model/Points.java
+++
@@ -1,7 +0,0 @@
-package ru.ifmo.se.web4.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Points extends JpaRepository<Point, Long> {
-
-}
--- web4/src/main/java/ru/ifmo/se/web4/user/Users.java
+++
@@ -1,7 +0,0 @@
-package ru.ifmo.se.web4.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Users extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
