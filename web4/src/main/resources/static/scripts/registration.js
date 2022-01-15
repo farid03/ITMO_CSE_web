1e856e49c911e91f99c6bff6c8ee3f7995c18474
Farid Kurbanov <71608691+farid03@users.noreply.github.com>
2022-01-15 07:29:43 +0300

    Update registration.js

--- web4/src/main/resources/static/scripts/registration.js
+++ web4/src/main/resources/static/scripts/registration.js
@@ -12,20 +12,49 @@ var registrationForm = new Vue({
                 this.message = 'Заполните форму до конца!';
             } else {
                 this.message = '';
-                regApi.save({}, this.user).then(result => result.text().then(
-                    data => {
-                        if (data === "sucsess") {
-                            document.location.href = "http://localhost:11125/login?sucsess";
-                            this.message = "";
-                        } else if (data === "exists") {
-                            this.message = 'Пользователь с данным именем уже существует!';
-                        } else if (data === "null") {
-                            this.message = 'Заполните форму до конца!';
-                        } else {
-                            this.message = 'Что-то пошло не так...';
-                        }
+                regApi.save({}, this.user).then(result => {
+                    if (result.ok) {
+                        result.text().then(
+                            data => {
+                                if (data === "success") {
+                                    document.location.href = "http://localhost:11125/login?success";
+                                    this.message = "";
+                                } else {
+                                    this.message = 'Что-то пошло не так...';
+                                }
+                            }
+                        )
                     }
-                ))
+                }, error => {
+                    switch (error.status) {
+                        case 409:
+                            if (error.bodyText === "exists") {
+                                this.message = 'Пользователь с данным именем уже существует!';
+                            } else {
+                                console.log(result)
+                            }
+                            break;
+                        case 400:
+                            if (error.bodyText === "null") {
+                                this.message = 'Заполните форму до конца!';
+                            } else {
+                                console.log(result)
+                            }
+                            break;
+                        default:
+                            this.message = 'Что-то пошло не так... ' + error.status;
+                            console.log(result);
+                            break;
+                    }
+                })
+            }
+        },
+        validateLength: function () {
+            if (this.user.username.length >= 20) {
+                this.user.username = this.user.username.substr(0,19);
+            }
+            if (this.user.password.length >= 20) {
+                this.user.password = this.user.password.substr(0,19);
             }
         }
     }
