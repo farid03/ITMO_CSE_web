b6a4e8d729681b1545a6c10b75321516197c8a75
Farid Kurbanov <71608691+farid03@users.noreply.github.com>
2022-01-15 07:28:28 +0300

    Update RegistrationController.java

--- web4/src/main/java/ru/ifmo/se/web4/controller/RegistrationController.java
+++ web4/src/main/java/ru/ifmo/se/web4/controller/RegistrationController.java
@@ -24,34 +24,29 @@ public class RegistrationController {
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("/registration")
-    public String addUser(@RequestBody User user) {
+    public ResponseEntity<?> addUser(@RequestBody User user) {
         User userFromDB = users.findByUsername(user.getUsername());
 
         if (userFromDB != null) {
             System.out.println("User exists!");
             System.out.println(user);
-            return "exists";
+            return new ResponseEntity("exists", HttpStatus.CONFLICT);
         }
 
         if (user.getUsername().trim().equals("") || user.getPassword().trim().equals("")) {
-            return "null"; //на всякий случай...
+            return new ResponseEntity("null", HttpStatus.BAD_REQUEST); //на всякий случай...
         }
 
         user.setPassword(passwordEncoder.encode(user.getPassword().trim()));
         user.setUsername(user.getUsername().trim());
         users.save(user);
 
-        return "sucsess";
+        return new ResponseEntity("success", HttpStatus.OK);
     }
 
     @GetMapping("/users")
     public ResponseEntity<?> points() {
-        List usernameList = new ArrayList();
-        for (User user : users.findAll()) {
-            usernameList.add(user.getUsername());
-        }
-        //TODO переделать на нормальный запрос к бд (без цикла)
-        return new ResponseEntity<>(usernameList, HttpStatus.OK);
+        return new ResponseEntity<>(users.getUsernames(), HttpStatus.OK);
     }
 
 }
