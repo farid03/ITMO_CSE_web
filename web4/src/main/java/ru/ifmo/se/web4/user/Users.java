0a6c23423b599b732eaf3cb58639dde6ed8009c8
Farid Kurbanov <71608691+farid03@users.noreply.github.com>
2022-01-15 07:31:21 +0300

    Update Users.java

--- web4/src/main/java/ru/ifmo/se/web4/user/Users.java
+++ web4/src/main/java/ru/ifmo/se/web4/user/Users.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface Users extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    
+    @Query(value = "SELECT username FROM users_table;", nativeQuery = true)
+    List getUsernames();
 }
